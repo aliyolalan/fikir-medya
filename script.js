69166df2a9bb0b72f0b0293190a67c6192d9948e
Ali Yolalan <aliyolalan@outlook.com>
2024-11-04 15:39:00 +0300

    New page and styles is added.

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ window.addEventListener("load", () => {
     document.querySelector(".container").style.display = "block";
   }, 500); // 5 saniye bekleme süresi
 });
+
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener("focus", function () {
+    this.closest(".form-group").classList.add("focused");
+  });
+
+  input.addEventListener("blur", function () {
+    const inputValue = this.value;
+    if (inputValue === "") {
+      this.classList.remove("filled");
+      this.closest(".form-group").classList.remove("focused");
+    } else {
+      this.classList.add("filled");
+    }
+  });
+});
